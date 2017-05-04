46738f80044f0f8995a998eaa12aaff23db085d0
jzawar <zawarjayesh@yahoo.com>
2017-05-04 12:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,10 +113,10 @@ def makeWebhookResult(data, req):
     parameters = airesult.get("parameters")
     person = parameters.get('Person')
     city = parameters.get("geo-city")
-    returnedSpeech = suggestDeodrant(condition.get('text'), person, city)
+    #returnedSpeech = suggestDeodrant(condition.get('text'), person, city)
     #print(returnedSpeech)
     print("Response:")
-    print(returnedSpeech)
+    print(Speech)
 
     return {
         "speech": Speech,
